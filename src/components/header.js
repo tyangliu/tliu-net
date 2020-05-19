74e64cea6a429801f7b810f2ee995f5cb29f95fc
tyangliu <tyangliu@gmail.com>
2020-05-18 22:19:32 -0700

    Complete logo animation cycle when re-focusing tab

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,24 @@ import React, {useState, useRef} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'gatsby';
 import {css, jsx} from '@emotion/core';
+import PageVisibility from 'react-page-visibility';
+
 import {ReactComponent as Logo} from 'src/images/logo.svg';
 
 const Header = ({siteTitle}) => {
   const [isHovering, setIsHovering] = useState(false);
   const [isRunning, setIsRunning] = useState(false);
+  const [isRefocused, setIsRefocused] = useState(false);
 
   const logoRef = useRef(null);
   const isHoveringRef = useRef(false);
+  const isRefocusedRef = useRef(false);
 
   isHoveringRef.current = isHovering;
+  isRefocusedRef.current = isRefocused;
 
   const pauseHandler = () => {
-    if (isHoveringRef.current) {
+    if (isHoveringRef.current || isRefocusedRef.current) {
       return;
     }
     setIsRunning(false);
@@ -25,130 +30,141 @@ const Header = ({siteTitle}) => {
       );
     }
   };
+  const focusHandler = isVisible => {
+    if (!isVisible) {
+      return;
+    }
+    setIsRefocused(true);
+    // When re-focusing tab, skip the next pauseHandler iteration
+    // to allow animation to complete from where it was paused after blur.
+    setTimeout(() => setIsRefocused(false), 10);
+  };
 
   return (
-    <header css={css`
-      padding: 0 19px;
-      background: rgb(255,255,255);
-    `}>
-      <div
-        css={css`
-          max-width: 1100px;
-          margin: 0 auto;
-          padding: 28px 0 24px;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-        `}
-      >
-        <h1 css={css`
-          margin: 0;
-          line-height: 1em;
+    <PageVisibility onChange={focusHandler}>
+      <header css={css`
+        padding: 0 19px;
+        background: rgb(255,255,255);
+      `}>
+        <div
+          css={css`
+            max-width: 1100px;
+            margin: 0 auto;
+            padding: 28px 0 24px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          `}
+        >
+          <h1 css={css`
+            margin: 0;
+            line-height: 1em;
 
 
-          #tliu-top {
-          }
-        `}>
-          <Link to='/' draggable={false}>
-            <div
-              ref={logoRef}
-              onMouseEnter={() => {
-                setIsHovering(true);
-                setIsRunning(true);
-              }}
-              onMouseLeave={() => {
-                setIsHovering(false);
-                if (logoRef.current) {
-                  logoRef.current.addEventListener(
-                    'animationiteration',
-                    pauseHandler,
-                  );
-                }
-              }}
-              css={css`
-                @keyframes tliu-top {
-                  0% {
-                    transform: rotate(0deg);
+            #tliu-top {
+            }
+          `}>
+            <Link to='/' draggable={false}>
+              <div
+                ref={logoRef}
+                onMouseEnter={() => {
+                  setIsHovering(true);
+                  setIsRunning(true);
+                }}
+                onMouseLeave={() => {
+                  setIsHovering(false);
+                  if (logoRef.current) {
+                    logoRef.current.addEventListener(
+                      'animationiteration',
+                      pauseHandler,
+                    );
                   }
-                  10% {
-                    transform: rotate(-2deg);
+                }}
+                css={css`
+                  @keyframes tliu-top {
+                    0% {
+                      transform: rotate(0deg);
+                    }
+                    10% {
+                      transform: rotate(-2deg);
+                    }
+                    25% {
+                      transform: rotate(-8deg);
+                    }
+                    40% {
+                      transform: rotate(-13deg);
+                    }
+                    50% {
+                      transform: rotate(-15deg);
+                    }
+                    60% {
+                      transform: rotate(-13deg);
+                    }
+                    75% {
+                      transform: rotate(-8deg);
+                    }
+                    90% {
+                      transform: rotate(-2deg);
+                    }
+                    100% {
+                      transform: rotate(0deg);
+                    }
                   }
-                  25% {
-                    transform: rotate(-8deg);
-                  }
-                  40% {
-                    transform: rotate(-13deg);
-                  }
-                  50% {
-                    transform: rotate(-15deg);
-                  }
-                  60% {
-                    transform: rotate(-13deg);
-                  }
-                  75% {
-                    transform: rotate(-8deg);
-                  }
-                  90% {
-                    transform: rotate(-2deg);
-                  }
-                  100% {
-                    transform: rotate(0deg);
-                  }
-                }
-                animation: tliu-top 2s linear infinite;
-                animation-play-state: ${isRunning ? 'running': 'paused'};
-              `}
-            >
-              <Logo height={42}/>
-            </div>
-          </Link>
-        </h1>
-        <nav css={css`
-          a {
-            color: #6e7175;
-            font-size: 16px;
-            font-weight: bold;
-            font-style: italic;
-            text-decoration: none;
-            margin-left: 3em;
-            display: inline-block;
-            position: relative;
-
-            &:hover {
+                  animation: tliu-top 2s linear infinite;
+                  animation-play-state: ${isRunning ? 'running': 'paused'};
+                `}
+              >
+                <Logo height={42}/>
+              </div>
+            </Link>
+          </h1>
+          <nav css={css`
+            a {
+              color: #6e7175;
+              font-size: 16px;
+              font-weight: bold;
+              font-style: italic;
               text-decoration: none;
-            }
+              margin-left: 3em;
+              display: inline-block;
+              position: relative;
 
-            &::after {
-              content: "—";
-              position: absolute;
-              top: 50%;
-              left: 0;
-              right: 0;
-              margin: auto;
-              width: 20px;
-              opacity: 0;
-              transition: opacity 0.2s;
-            }
+              &:hover {
+                text-decoration: none;
+              }
 
-            &.active::after, &:hover::after {
-              opacity: 1;
+              &::after {
+                content: "—";
+                position: absolute;
+                top: 50%;
+                left: 0;
+                right: 0;
+                margin: auto;
+                width: 20px;
+                opacity: 0;
+                transition: opacity 0.2s;
+              }
+
+              &.active::after, &:hover::after {
+                opacity: 1;
+              }
             }
-          }
-        `}>
-          {/*
-          <Link to='/lab' activeClassName='active' draggable={false}>
-            Lab
-          </Link>
-          <Link to='/photos' activeClassName='active' draggable={false}>
-            Photos
-          </Link>
-          */}
-          <Link to='/about' activeClassName='active' draggable={false}>
-            About
-          </Link>
-        </nav>
-      </div>
-    </header>
+          `}>
+            {/*
+            <Link to='/lab' activeClassName='active' draggable={false}>
+              Lab
+            </Link>
+            <Link to='/photos' activeClassName='active' draggable={false}>
+              Photos
+            </Link>
+            */}
+            <Link to='/about' activeClassName='active' draggable={false}>
+              About
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </PageVisibility>
   );
 };
 
